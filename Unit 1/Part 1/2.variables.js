33c7f3c1c796df3f22e4cf1a7528e1b9dcb9b586
Arturo Bernal <arturober@gmail.com>
2020-09-16 14:02:51 +0200

    16/9

---
+++ Unit 1/Part 1/2.variables.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let a = 12;
+console.log(a); // 12
+console.log(typeof a); // number
+a = "Hello";
+console.log(a); // Hello
+console.log(typeof a); // string
+
+let b = 20;
+console.log(b);
+
+const con = 99;
+// con = 10; -> ERROR
+
---
+++ Unit 1/Part 1/3.functions.js
@@ -0,0 +1,10 @@
+function random(num) {
+    if(num < 10) {
+        return num * 2;
+    }
+}
+
+console.log(random(3)); // 6
+console.log(random(26)); // undefined
+
+
