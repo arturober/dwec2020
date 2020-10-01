ecb66fc9788b84ad615762583f3f24e3c33ae390
Arturo Bernal <arturober@gmail.com>
2020-10-01 11:41:56 +0200

    01/10

--- Unit 1/Part 2/18.products/products.js
+++ Unit 1/Part 2/18.products/products.js
@@ -16,8 +16,48 @@ function convertBase64(file) {
     });
 }
 
+function addProduct(e) {
+    e.preventDefault();
+    let name = productForm.name.value;
+    let desc = productForm.description.value;
+    let photo = imagePreview.src;
+
+    let tr = document.createElement("tr");
+    let tdPhoto = document.createElement("td");
+    let img = document.createElement("img");
+    img.src = photo;
+    tdPhoto.appendChild(img);
+    tr.appendChild(tdPhoto);
+
+    let tdName = document.createElement("td");
+    tdName.innerText = name;
+    tr.appendChild(tdName);
+
+    let tdDesc = document.createElement("td");
+    tdDesc.innerText = desc;
+    tr.appendChild(tdDesc);
+
+    let tdDelete = document.createElement("td");
+    let btnDelete = document.createElement("button");
+    btnDelete.classList.add("delete");
+    btnDelete.innerText = "Delete";
+    btnDelete.addEventListener("click", e => tr.remove());
+    tdDelete.appendChild(btnDelete);
+    tr.appendChild(tdDelete);
+
+    tableProducts.querySelector("tbody").appendChild(tr);
+    productForm.reset();
+    imagePreview.src = "";
+}
+
 document.addEventListener("DOMContentLoaded", e => {
     imagePreview = document.getElementById("imagePreview");
     productForm = document.getElementById("addProduct");
     tableProducts = document.getElementById("products");
+
+    productForm.photo.addEventListener("change", e => {
+        convertBase64(productForm.photo.files[0]);
+    });
+
+    productForm.addEventListener("submit", addProduct);
 });
