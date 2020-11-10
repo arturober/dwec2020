94e452dd374d5ab639d85f8863a50eabe421e003
Arturo Bernal <arturober@gmail.com>
2020-11-10 11:56:16 +0100

    10/11

--- Unit 2/Part 1/angular-products/src/app/product-list/product-list.component.ts
+++ Unit 2/Part 1/angular-products/src/app/product-list/product-list.component.ts
@@ -41,12 +41,46 @@ export class ProductListComponent implements OnInit {
     rating: 1
   }];
 
+  newProduct!: Product;
+  imageFile = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.resetProduct();
   }
 
   toggleImages(): void {
     this.showImages = !this.showImages;
+  }
+
+  loadImage(input: HTMLInputElement): void {
+    if (!input.files) { return; }
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    if (file) { // File has been selected (convert to Base64)
+      reader.readAsDataURL(file);
+    }
+
+    reader.addEventListener('load', () => { //Converted into Base64 event (async)
+      this.newProduct.imageUrl = reader.result as string;
+    });
+  }
+
+  addProduct(): void {
+    this.products.push(this.newProduct);
+    this.resetProduct();
+  }
+
+  resetProduct(): void {
+    this.newProduct = {
+      description: '',
+      available: '',
+      imageUrl: '',
+      price: 0,
+      rating: 0
+    };
+    this.imageFile = '';
   }
 }
